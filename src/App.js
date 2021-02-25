c5805d6033046bd69659d2fd7296ceee36ebd409
Amandeep Singh <ec2-user@ip-172-31-55-248.ec2.internal>
2021-02-25 14:55:13 +0000

    Added calculateWinner and prevent users from entering moves after the board is won

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
   
   function addMove(index){
     
+    if (calculateWinner(board)){
+      return;
+    }
     if (board[index] === ""){
       if (turn === 0) {
         setBoard(prevBoard => Object.assign([...prevBoard], {[index]: "X"} ));
@@ -36,6 +39,30 @@ function App() {
     setBoard(prevBoard => Object.assign([...prevBoard], {[data.index]: data.play }));
     });
   }, []);
+  
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+      else if(!squares.includes("")){
+        return 'draw';
+      }
+    }
+    return null;
+  }
   
   
   return (
