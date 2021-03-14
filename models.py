2ddd524445d0f98214e167713440855c45efd86a
Amandeep Singh <ec2-user@ip-172-31-55-248.ec2.internal>
2021-03-14 15:50:17 +0000

    Added docstring to models

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
+'''Creates a class to use for the DB'''
 from app import DB
 
 class Player(DB.Model):
+    ''' The DB entries will have id, username, and points '''
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
     points = DB.Column(DB.Integer, nullable=False)
