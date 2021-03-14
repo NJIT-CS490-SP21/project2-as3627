6dc458cc532a8d377c27b6de0c75e0e4ca5047e9
Amandeep Singh <ec2-user@ip-172-31-55-248.ec2.internal>
2021-03-14 21:08:52 +0000

    Added another unit test involving login function

--- tests/unmocked/unit_tests.py
+++ tests/unmocked/unit_tests.py
@@ -63,6 +63,7 @@ class FormatListTestCase(unittest.TestCase):
             self.assertNotEqual(actual_result, expected_result)
 
 
+# I took the same format_list function from app.py
 def format_list(old):
     new_list = []
     for value in enumerate(old):
@@ -74,7 +75,99 @@ def format_list(old):
         new_list.append(temp)
 
     return new_list
+
+
+
+class LoginTestCase(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: ["Player_A"],
+                KEY_EXPECTED: [["Player_A"],[]],
+            },
+
+            {
+                KEY_INPUT: ["Player_A", "Player_B"],
+                KEY_EXPECTED: [["Player_A", "Player_B"], []],
+            },
+
+            {
+                KEY_INPUT: ["Player_A", "Player_B", "Spectator_1", "Spectator_2"],
+                KEY_EXPECTED: [["Player_A", "Player_B"], ["Spectator_1", "Spectator_2"]],
+            },
+
+            {
+                KEY_INPUT: ["Player_A", "Player_A", "Player_A"],
+                KEY_EXPECTED: [["Player_A"],[]],
+            },
+        ]
+        
+        self.failure_test_params = [
+            {
+                KEY_INPUT: ["Player_A", "Player_B"],
+                KEY_EXPECTED: [["Player_A"],["Player_B"]],
+            },
+            {
+                KEY_INPUT: ["Player_A", "Player_B", "Player_C"],
+                KEY_EXPECTED: [["Player_A", "Player_B", "Player_C"], []],
+            },
+            {
+                KEY_INPUT: ["Spectator_1", "Spectator_2", "Spectator_3"],
+                KEY_EXPECTED: [[], ["Spectator_1", "Spectator_2", "Spectator_3"]],
+            },
+            {
+                KEY_INPUT: ["Spectator_1", "Spectator_1", "Spectator_1"],
+                KEY_EXPECTED: [["Spectator_1", "Spectator_1"], ["Spectator_1"]],
+            },
             
+        ]
+
+
+    def test_login_success(self):
+        global PLAYERS, SPECTATORS
+        for test in self.success_test_params:
+            for i in test[KEY_INPUT]:
+                on_login(i)
+            
+            expected_result = test[KEY_EXPECTED]
+            self.assertEqual(len(PLAYERS), len(expected_result[0]))
+            self.assertEqual(PLAYERS, expected_result[0])
+            self.assertEqual(len(SPECTATORS), len(expected_result[1]))
+            self.assertEqual(SPECTATORS, expected_result[1])
+            PLAYERS = []
+            SPECTATORS = []
+            
+    def test_login_failure(self):
+        global PLAYERS, SPECTATORS
+        for test in self.failure_test_params:
+            for i in test[KEY_INPUT]:
+                on_login(i)
+            
+            expected_result = test[KEY_EXPECTED]
+            self.assertNotEqual(len(PLAYERS), len(expected_result[0]))
+            self.assertNotEqual(PLAYERS, expected_result[0])
+            self.assertNotEqual(len(SPECTATORS), len(expected_result[1]))
+            self.assertNotEqual(SPECTATORS, expected_result[1])
+            PLAYERS = []
+            SPECTATORS = []
+
+
+PLAYERS = []
+SPECTATORS = []
+
+# I rewrote the login function so it doesn't involve DB or SocketIO
+def on_login(name):
+    '''When a client logs in'''
+    if name in PLAYERS:
+        return
+    elif name in SPECTATORS:
+        return
+    else:
+
+        if len(PLAYERS) < 2:
+            PLAYERS.append(name)
+        else:
+            SPECTATORS.append(name)
 
 
 if __name__ == '__main__':
